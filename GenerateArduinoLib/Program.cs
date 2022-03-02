bd0cc1b74a69c9405611b0650670ac8a06b408d5
Gary Eades <gary.eades@inmechasol.com>
2022-03-02 12:15:28 -0500

    Adding Matrix Orbital MOP device and updates via auto doc tools

--- GenerateArduinoLib/Program.cs
+++ GenerateArduinoLib/Program.cs
@@ -25,6 +25,113 @@ namespace GenerateArduinoLib
             System.Threading.Thread.Sleep(1); // This makes the difference between whether it works or not. Sleep(0) is not enough.
             Directory.Delete(target_dir);
         }
+        public static string LicenseText()
+        {
+            return @"
+    Copyright 2021 InMechaSol, Inc
+    https://www.inmechasol.org/
+
+    Licensed under the Apache License, Version 2.0(the ""License"");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an ""AS IS"" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.";
+        }
+        public static void FixFileBriefNLicense(string fstring)
+        {
+            bool fileNeedsNewHeaderText = true;
+            Console.WriteLine("Processing: " + fstring);
+            string fileText = File.ReadAllText(fstring);
+
+
+            string ftypeText = "HowUnlikelyWouldItBe@if thisEx@ctTextWasN8";
+            if (Path.GetExtension(fstring) == ".h")
+            {
+                ftypeText = "Declarations for straight C and C++ wrappers";
+            }
+            else if (Path.GetExtension(fstring) == ".cpp")
+            {
+                ftypeText = "Implementation for C++ wrappers";
+            }
+            else if (Path.GetExtension(fstring) == ".c")
+            {
+                ftypeText = "Implementation for straight C, or Declarations for C++ wrappers";
+            }
+
+
+            if (fileText.StartsWith("/** \\file " + Path.GetFileName(fstring)))
+            { 
+                if(fileText.Contains("\r\n*   \\brief part of ccNOos, "))
+                { 
+                    if(fileText.Contains(ftypeText))
+                    { 
+                        if(fileText.Contains("\r\n\r\nNotes:"))
+                        { 
+                            if ( fileText.Contains("(.c includes .h) - for straight C") && fileText.Contains("(.cpp includes .c which includes .h) - for C++ wrapped straight C") && fileText.Contains("Always compiled to a single compilation unit, either C or CPP, not both"))
+                            {
+                                if (fileText.Contains("Copyright 2021 InMechaSol, Inc") && fileText.Contains("https://www.inmechasol.org/"))
+                                {
+                                    if (fileText.Contains("Licensed under the Apache License, Version 2.0(the \"License\");") && fileText.Contains("you may not use this file except in compliance with the License.") && fileText.Contains("You may obtain a copy of the License at"))
+                                    {
+                                        if (fileText.Contains("http://www.apache.org/licenses/LICENSE-2.0"))
+                                        {
+                                            if (fileText.Contains("Unless required by applicable law or agreed to in writing, software") &&
+                                                fileText.Contains("distributed under the License is distributed on an \"AS IS\" BASIS,") &&
+                                                fileText.Contains("WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.") &&
+                                                fileText.Contains("See the License for the specific language governing permissions and") &&
+                                                fileText.Contains("limitations under the License.")
+                                                )
+                                            {
+                                                fileNeedsNewHeaderText = false;
+            }   }   }   }   }   }   }   }   }
+
+            if(fileNeedsNewHeaderText)
+            {
+                //if(Path.GetExtension(fstring)==".c")
+                //{
+                //    fileText = fileText.Substring(fileText.IndexOf("#"));
+                //}
+                //else if (Path.GetExtension(fstring) == ".cpp")
+                //{
+                //    fileText = fileText.Substring(fileText.IndexOf("#include"));
+                //}
+                //if (Path.GetExtension(fstring) == ".h")
+                //{
+                int j = fileText.IndexOf("\n#");
+                int k = fileText.IndexOf("\n/*");
+                int i = fileText.IndexOf("*/");
+                int h = 0;
+                if (j > -1)// first compiler statement (#ifndef, #include, ... ) 
+                {
+                    if (k>-1)// opened comment
+                    {
+                        if(i>k&&i<j)// closed comment before first compiler statement
+                        {
+                            h = j - 1; // clear from compiler statement to top of file
+                        }
+                    } 
+                    else// never opened comment
+                    {
+                        h = j - 1; // clear from compiler statement to top of file
+                    }
+                }
+                fileText = fileText.Substring(h);
+                //}
+                fileText = fileText.Substring(fileText.IndexOf("\n#"));
+                fileText = "/** \\file " + Path.GetFileName(fstring) + "\r\n*   \\brief part of ccNOos, "+ ftypeText + " \r\n" + LicenseText() + "\r\n\r\nNotes:\r\n\t(.c includes .h) - for straight C or\r\n\t(.cpp includes .c which includes .h) - for C++ wrapped straight C\r\n\t*Always compiled to a single compilation unit, either C or CPP, not both\r\n\r\n*/\r\n" + fileText;
+
+
+
+                File.WriteAllText(fstring, fileText);
+               
+            }
+        }
 
         // this is called for each directory in the ccNOos directory
         public static void CopyModifyDirectoryRecursively(string target_dir, string dirstring)
@@ -34,11 +141,11 @@ namespace GenerateArduinoLib
             {
                 // fstring - path to source file
                 // filestring - path to destination file
-                string filestring = dirstring + "\\" + fstring.Substring(fstring.LastIndexOf("\\")+1);
-                
+                string filestring = dirstring + "\\" + fstring.Substring(fstring.LastIndexOf("\\") + 1);
+
                 if (Path.GetExtension(fstring).Equals(".c"))
-                {
-                    Console.WriteLine("Processing: " + fstring);
+                {                    
+                    FixFileBriefNLicense(fstring);
                     // copy and change extension to hpp
                     File.Copy(fstring, filestring.Replace(".c", ".hpp"));
                 }
@@ -80,27 +187,27 @@ namespace GenerateArduinoLib
                     //}
                     //else
                     //{
-                        Console.WriteLine("Processing: " + fstring);
-                        // copy and modify #include line within
-                        string ftext = File.ReadAllText(fstring);
+                    FixFileBriefNLicense(fstring);
+                    // copy and modify #include line within
+                    string ftext = File.ReadAllText(fstring);
 
-                        Regex rx = new Regex(@"#include .*\.([c])");
-                        MatchCollection matches = rx.Matches(ftext);
-                        foreach (Match m in matches)
-                        {
-                            string[] toks = m.Value.Split('.');
-                            ftext = rx.Replace(ftext, toks[0] + ".hpp");
-                        }
-                        File.WriteAllText(filestring, ftext);
+                    Regex rx = new Regex(@"#include .*\.([c])");
+                    MatchCollection matches = rx.Matches(ftext);
+                    foreach (Match m in matches)
+                    {
+                        string[] toks = m.Value.Split('.');
+                        ftext = rx.Replace(ftext, toks[0] + ".hpp");
+                    }
+                    File.WriteAllText(filestring, ftext);
                     //}
-                    
+
                 }
                 else if (Path.GetExtension(fstring).Equals(".h") || Path.GetExtension(fstring).Equals(".ino"))
                 {
-                    Console.WriteLine("Processing: " + fstring);
+                    FixFileBriefNLicense(fstring);
                     if (!fstring.Contains("Platform"))
                         File.Copy(fstring, filestring);
-                    else if(fstring.Contains("Arduino"))
+                    else if (fstring.Contains("Arduino"))
                         File.Copy(fstring, filestring);
                     else if (fstring.Contains("PlatformApp_Serialization"))
                         File.Copy(fstring, filestring);
@@ -131,11 +238,11 @@ namespace GenerateArduinoLib
             // recursively process each sub-directory
             foreach (string subDir in Directory.GetDirectories(target_dir))
             {
-                if(!subDir.Contains("tests\\testApps") && !subDir.Contains("tests\\testMainFiles"))
+                if (!subDir.Contains("tests\\testApps") && !subDir.Contains("tests\\testMainFiles"))
                     CopyModifyDirectoryRecursively(subDir, dirstring);
-                else if(subDir.Contains("tests\\testApps"))
+                else if (subDir.Contains("tests\\testApps"))
                 {
-                    if(subDir.Contains("apiModules") || subDir.Contains("deviceModules"))
+                    if (subDir.Contains("apiModules") || subDir.Contains("deviceModules"))
                     {
                         CopyModifyDirectoryRecursively(subDir, dirstring);
                     }
@@ -144,9 +251,9 @@ namespace GenerateArduinoLib
                         string tempDirstring = subDir + "\\" + dirstring.Substring(dirstring.LastIndexOf("\\") + 1);
                         CopyModifyDirectoryRecursively(tempDirstring, dirstring);
                     }
-                    
+
                 }
-                else if(subDir.Contains("tests\\testMainFiles"))
+                else if (subDir.Contains("tests\\testMainFiles"))
                 {
                     string tempDirstring = subDir + "\\" + dirstring.Substring(dirstring.LastIndexOf("\\") + 1) + "\\Arduino";
                     CopyModifyDirectoryRecursively(tempDirstring, dirstring);
@@ -154,7 +261,6 @@ namespace GenerateArduinoLib
             }
 
         }
-
         static void Main(string[] args)
         {
             // Program expects ccNOosTest Directory as an input
